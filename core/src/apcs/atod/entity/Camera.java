a0ebabf424a45a27aa4301cf90e5804451c0b5e0
Mikesnake5 <michaelm22094@stu.powayusd.com>
2016-05-12 08:38:01 -0700

    final conflict plz

--- core/src/apcs/atod/entity/Camera.java
+++ core/src/apcs/atod/entity/Camera.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package apcs.atod.entity;
 
 public class Camera extends Entity
@@ -13,19 +13,4 @@ public class Camera extends Entity
 		
 	}
 }
-=======
-package apcs.atod.entity;
 
-public class Camera extends Entity
-{
-	public void setup()
-	{
-		
-	}
-	
-	public void update()
-	{
-		
-	}
-}
->>>>>>> a904973439dac13c9624dc0bd24c0e6bf76d92f5
