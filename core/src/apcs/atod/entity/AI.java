7b786f3e8aa29f7b119e7c2cb06535ef591ad594
Mikesnake5 <michaelm22094@stu.powayusd.com>
2016-05-26 09:10:07 -0700

    got old file back

--- core/src/apcs/atod/entity/AI.java
+++ core/src/apcs/atod/entity/AI.java
@@ -22,7 +22,7 @@ public class AI extends Entity
 	private int x = 2;
 	private float rotation;
 	private int l = 0;
-	private int m = -10;
+	private int m = 0;
 	private int n = 0;
 
 	
@@ -56,8 +56,10 @@ public class AI extends Entity
 		damage = 1;
 		hp = Math.random() * 10 + 10;
 		rof = 1;
+
 		caseNum = 0;
-		rotation = 3f;
+
+		rotation = 20f;
 		finishManuever = 0;
 		//pos = new Vector3(1f, 1f, 1f); //will fix this later
 		//endPosition = new Vector3(-1f, -1f, -1f); //same
@@ -69,37 +71,40 @@ public class AI extends Entity
 
 		if (hp > 0)
 		{
-			dodge5(finishManuever);
-		}
-		//	if (finishManuever == 0)
-			//	caseNum = (int) (1 + Math.random() * 6);
-			
-		//	switch(caseNum)
-//			{
-		//	case 1 :
-		//		flyTowardsPlayer(finishManuever);
-		//		break;
-		//	case 2 :
-		//		doABarrelRoll(finishManuever);
-		//		break;
-		//	case 3 :
-		//		dodge1(finishManuever);
-		//		break;
-		//	case 4 :
-		//		dodge2(finishManuever);
-		//		break;
-		//	case 5 :
-		//		dodge3(finishManuever);
-		//		break;
-		//	case 6 :
-		//		dodge4(finishManuever);
-		//		break;
-		//	}
-			finishManuever++;
 
-	//	}
-	//	else
-	//		die(finishManuever);
+			//dodge2(FinishManuever);
+			//FinishManuever++;
+			if (finishManuever == 0)
+				caseNum = (int) (1 + Math.random() * 6);
+			finishManuever++;
+			switch(caseNum)
+			{
+			case 1 :
+				flyTowardsPlayer(finishManuever);
+				break;
+			case 2 :
+				doABarrelRoll(finishManuever);
+				break;
+			case 3 :
+				dodge1(finishManuever);
+				break;
+			case 4 :
+				dodge2(finishManuever);
+				break;
+			case 5 :
+				dodge3(finishManuever);
+				break;
+			case 6 :
+				dodge4(finishManuever);
+				break;
+			case 7 :
+				dodge5(finishManuever);
+			}
+
+
+		}
+		else
+			die(finishManuever);
 
 	}
 	
@@ -129,7 +134,9 @@ public class AI extends Entity
 	
 	private void dodge1(int x)
 	{
+
 		modelInstance.transform.rotate(0, 0, 1,(5));
+
 	}
 	
 	private void dodge2(int x)
@@ -155,7 +162,7 @@ public class AI extends Entity
 			{
 				modelInstance.transform.rotate(0, 0, 1,-5);
 			}
-		finishManuever++;
+		
 		if (x >= 110)
 			finishManuever = 0;
 		
@@ -183,10 +190,11 @@ public class AI extends Entity
 			{
 				modelInstance.transform.rotate(0, 0, 1,5);
 			}
+
+		
 		if (x >= 110)
-		{
 			finishManuever = 0;
-		}
+
 	}
 	private void dodge4 (int x)
 	{
@@ -204,40 +212,59 @@ public class AI extends Entity
 		{
 			finishManuever = 0;
 		}
-		finishManuever++;
+		
 		if (x >= 60)
 			finishManuever = 0;
 	}
 	private void dodge5 (int x)
 	{
-		if (x > 0 && x < 8)
+		if (x > 0 && x < 50)
 		{
-			modelInstance.transform.rotate(1,0,0,-5);
-			modelInstance.transform.translate(new Vector3(0,40,-40));
+
+			modelInstance.transform.translate(new Vector3(l,m,n));
+			modelInstance.transform.rotate(pos.Z,90);
+			switch(x % 4)
+			{
+			case 0:
+				l += 10;
+				m += 10;
+			case 1:
+				m -= 10;
+				l += 10;
+			case 2:
+				l -= 10;
+				m -= 10;
+			case 3:
+				m += 10;
+				l -= 10;
+			}
+			
+
 		}
-		if (x > 10 && x < 30)
-		{
-			modelInstance.transform.rotate(1,0,0, 5);
-			modelInstance.transform.translate(new Vector3(0,20,20));
-		}
-		if (x > 30	&& x < 42)
-		{
-			modelInstance.transform.rotate(1,0,0,-5);
-			modelInstance.transform.translate(new Vector3(0,-12, 12));
-		}
-		if(x > 100)
-			finishManuever = 0;	
+		else
+			finishManuever = 0;
 	}
 	
+	private void doABarrelRoll(int x)
+	{
+		if (x < 60)
+			modelInstance.transform.rotate(pos.Z, rotation);
+		else
+			finishManuever = 0;
+	}
+	
+
 	private void flyTowardsPlayer(int x)
 	{
 		// fly towards the player
 		if (x < 60)
 		{
-			modelInstance.transform.translate(new Vector3(0f, 0f, 10f));
-			finishManuever++;
+			modelInstance.transform.translate(new Vector3(0f, 10f, 10f));
+			
 		}
 		else
 			finishManuever = 0;
+
 	}
+
 }
